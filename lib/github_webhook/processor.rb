42bd95451efd2126af9038d12a7299fd3ae42261
Andre Arko <andre@arko.net>
2017-07-22 19:02:43 -0700

    sort events and give each a line for diffs

--- lib/github_webhook/processor.rb
+++ lib/github_webhook/processor.rb
@@ -20,7 +20,45 @@ module GithubWebhook::Processor
   #     }
   #   });
   #   console.log(events);
-  GITHUB_EVENTS_WHITELIST = %w(ping commit_comment create delete deployment deployment_status download follow fork fork_apply gist gollum issue_comment issues label member membership milestone organization org_block page_build project_card project_column project public pull_request pull_request_review pull_request_review_comment push release repository status team team_add watch integration_installation integration_installation_repositories)
+  GITHUB_EVENTS_WHITELIST = %w(
+    commit_comment
+    create
+    delete
+    deployment
+    deployment_status
+    download
+    follow
+    fork
+    fork_apply
+    gist
+    gollum
+    integration_installation
+    integration_installation_repositories
+    issues
+    issue_comment
+    label
+    member
+    membership
+    milestone
+    organization
+    org_block
+    page_build
+    ping
+    project
+    project_card
+    project_column
+    public
+    pull_request
+    pull_request_review
+    pull_request_review_comment
+    push
+    release
+    repository
+    status
+    team
+    team_add
+    watch
+  )
 
   def create
     if self.respond_to? event_method
